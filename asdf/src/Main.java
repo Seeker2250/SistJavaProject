f95d55cd5ea8c32f8bed3271991c88067e1f21b7
Seeker2250 <a01044777016@gmail.com>
2024-07-31 12:20:54 +0900

    test

---
+++ asdf/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author created by Seeker2250 on 7/31/24.
+ * @Subject
+ * @Contents
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
